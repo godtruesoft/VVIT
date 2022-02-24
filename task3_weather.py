755c0b9953aa81f0d84c08e3b2d8ade02250c28e
stalkozlov <43998044+stalkozlov@users.noreply.github.com>
2022-02-24 21:57:17 +0300

    Add files via upload

---
+++ task3_weather.py
@@ -0,0 +1,20 @@
+import requests
+
+city = "Moscow,RU"
+appid = "a63fc75c2ed2476cf4e7e01b3e83d9f7"
+
+res = requests.get("http://api.openweathermap.org/data/2.5/weather",
+params={'q': city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+data = res.json()
+print("Погода на текущее время:")
+print ("\t",data["weather"][0]["description"],". Скорость ветра: ",data['wind']["speed"],"м/с")
+
+res = requests.get("http://api.openweathermap.org/data/2.5/forecast",
+params={'q': city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+data = res.json()
+
+print("Погода на неделю:")
+for i in data["list"]:
+    print(i["dt_txt"],":")
+    print ("\t",i["weather"][0]["description"],". Скорость ветра: ",i['wind']["speed"],"м/с")
+
